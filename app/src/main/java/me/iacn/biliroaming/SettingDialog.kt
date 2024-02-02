463046db4d18fb97f01fa12b78f6f033de7b40f2
Sakari <20642596+sakarie9@users.noreply.github.com>
2024-02-02 19:14:58 +0800

    feat: add back MusicNotification

--- app/src/main/java/me/iacn/biliroaming/SettingDialog.kt
+++ app/src/main/java/me/iacn/biliroaming/SettingDialog.kt
@@ -247,7 +247,7 @@ class SettingDialog(context: Context) : AlertDialog.Builder(context) {
 
         private fun checkCompatibleVersion() {
             val versionCode = getVersionCode(packageName)
-            var supportMusicNotificationHook = versionCode >= 7500300 &&
+            var supportMusicNotificationHook =
                     // from bilibili
                     Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && !Build.MANUFACTURER.lowercase().equals("huawei")
             var supportCustomizeTab = true
